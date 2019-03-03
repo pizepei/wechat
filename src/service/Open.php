93760e14cf1a7fb94289e9a5bce6bbf66501731f
pizepei <848733913@qq.com>
2019-03-04 07:21:55 +0800

    微信第三方平台

--- src/service/Open.php
+++ src/service/Open.php
@@ -283,18 +283,14 @@ class Open
      * @throws \Exception
      *
      * @title  获取授权方authorizer_access_token
-     * @explain $restartw 为false时强制获取
-     * @authTiny 微权限提供权限分配 [获取店铺所有  获取所有店铺  获取一个]
-     * @authGroup 权限分组对应文件头部 @authGroup
+     * @explain $restartw 为true时强制获取
      *
-     * @router 方法路由一般控制器只适应(get /user/:user_id[int] ))
      */
-    public static function authorizer_access_token($authorizerAppid,$authorizerRefreshToken,$restart=true)
+    public static function authorizer_access_token($authorizerAppid,$authorizerRefreshToken,$restart=false)
     {
         $result = self::$Redis->get(self::$Config['prefix'].$authorizerAppid.'_authorizer_access_token');
 
         if(empty($result) || $restart){
-
             $postData = [
                 "component_appid"=>self::$Config['appid'],
                 "authorizer_appid"=>$authorizerAppid,
